3ef7d3289efd77fc03371d08e638d892161bf270
Nguyen Nhat Kha <nhatkhaa310@gmail.com>
2023-12-25 14:02:19 +0700

    new features

--- edit.c
+++ edit.c
@@ -5,11 +5,16 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
+int sum(int a,int b){
+ return a+b;
+}
 int main()
 {
     int a = 10, b = 5;
     swap(&a, &b);
+    int result_sum=sum(a,b);
     printf("A = %d\r\n", a);
     printf("B = %d\n", b);
+    printf("Result = %d \n",result_sum);
     return 0;
 }
